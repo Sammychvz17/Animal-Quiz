d29b376162c03fa62068be4464d8039e5bf7cfc8
sammychvz <sammy_chvz17@outlook.com>
2021-12-05 22:35:58 -0600

    added var in java and click

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var userName = document.querySelector("#userName");
+var headerText = document.querySelector("#headerText");
+var startGame = document.querySelector("#start-game");
+var homePage = document.querySelector("home-page");
+
+ function greetingPage () {
+    //homePage.innerHTML = " ";
+    homePage.innerHTML = "Hello " + userName.value + " , Welcome to my quiz.";
+
+};
+
+
+//Event Listener at End of JS 
+
+
