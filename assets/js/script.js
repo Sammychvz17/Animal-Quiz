e3294db4b4c02678889b4feb3fb69c64ef2de384
sammychvz <sammy_chvz17@outlook.com>
2021-12-06 19:48:02 -0600

    added event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,14 @@ var headerText = document.querySelector("#headerText");
 var startGame = document.querySelector("#start-game");
 var homePage = document.querySelector("home-page");
 
- function greetingPage () {
-    //homePage.innerHTML = " ";
-    homePage.innerHTML = "Hello " + userName.value + " , Welcome to my quiz.";
-
+ function greetingPage() {
+    console.log('Hey');
+   
 };
 
 
 //Event Listener at End of JS 
+
+startGame.addEventListener('submit', greetingPage);
 
 
