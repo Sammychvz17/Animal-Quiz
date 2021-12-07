d2834d2cee2578b4ddbcc24efa3f722a3151e30a
sammychvz <sammy_chvz17@outlook.com>
2021-12-06 22:26:06 -0600

    added second page to begin quiz

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,38 @@
 var userName = document.querySelector("#userName");
 var headerText = document.querySelector("#headerText");
-var startGame = document.querySelector("#start-game");
-var homePage = document.querySelector("home-page");
+var startBtn = document.querySelector("#start-btn");
+var homePage = document.querySelector("#home-page");
+var lionImg = document.querySelector("#lion");
 
- function greetingPage() {
-    console.log('Hey');
+
+ function startQuiz() {
+    
+    //removing elements from first page
+    homePage.remove()
+    lionImg.remove()
+
+    //showing the username in console 
+    console.log(userName.value);
+
+    //create new content 
+   const firstQuestionHolder = document.createElement('div');
+
+   const firstQuestion = document.createTextNode("TESTING");
+
+   firstQuestionHolder.appendChild(firstQuestion);
+
+    // add the newly created element and its content into the DOM
+  const currentDiv = document.getElementById("home-page");
+  document.body.insertBefore(firstQuestionHolder, currentDiv);
+    
+    
+
    
 };
 
 
 //Event Listener at End of JS 
 
-startGame.addEventListener('submit', greetingPage);
+startBtn.addEventListener('click', startQuiz);
 
 
